4104632607febcaa828c3bfad913743ee14570fb
jstaffans <johannes.staffans@gmail.com>
2015-08-29 01:18:20 +0200

    Output lists correctly, refactoring

--- src/galapagos/output.clj
+++ src/galapagos/output.clj
@@ -2,18 +2,31 @@
   (:require [clojure.walk :as walk])
   (:refer-clojure :exclude [spit]))
 
-(defn collect-fields [result]
-  (let [fields (:fields result)]
-    (into {} (walk/postwalk
-               #(if-let [solution (:solution  %)]
-                 (if (empty? (:fields %)) solution (collect-fields %))
-                 %)
-               fields))))
+(defn- many-results?
+  [result]
+  (vector? (:solution result)))
+
+(defn- leaf?
+  [field]
+  (empty? (:fields field)))
+
+(defn- collect-fields [result]
+  (let [fields (if (many-results? result)
+                 (map (partial into {}) (:fields result))
+                 (into {} (:fields result)))]
+    (walk/postwalk
+      (fn [field]
+        (if-let [solution (:solution field)]
+          (if (leaf? field)
+            solution
+            (collect-fields field))
+          field))
+      fields)))
 
 (defn collect
   "Collects a solved context into a simple map."
   [context]
   (let [root-key (first (keys context))]
-    {root-key (collect-fields (get context root-key))})  )
+    {root-key (collect-fields (get context root-key))}))
 
 
