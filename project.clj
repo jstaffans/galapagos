a9b729e3386b94c501d85845179ae6ff0e5c4794
jstaffans <johannes.staffans@gmail.com>
2015-08-29 22:57:06 +0200

    Add muse dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.wtfpl.net"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [instaparse "1.4.1"]
-                 [prismatic/plumbing "0.4.4"]
-                 [prismatic/schema "0.4.3"]]
+                 [prismatic/schema "0.4.3"]
+                 [muse "0.4.0"]
+                 [cats "0.4.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles {:dev {:dependencies [[juxt/iota "0.1.2"]]}})
