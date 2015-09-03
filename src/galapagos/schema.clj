a618fa3efe103f16510aa46314af47acc6604d0a
jstaffans <johannes.staffans@gmail.com>
2015-09-03 08:53:01 +0200

    Treat enums as plain schema.core/enums

--- src/galapagos/schema.clj
+++ src/galapagos/schema.clj
@@ -1,7 +1,7 @@
 (ns galapagos.schema
   (:require [schema.core :as s]
             [muse.core :as muse])
-  (:import (schema.core Predicate))
+  (:import (schema.core Predicate EnumSchema))
   (:refer-clojure :exclude [deftype]))
 
 (def GraphQLInt s/Int)
@@ -15,14 +15,12 @@
   (or
     (= Class (type t))
     (= Predicate (type t))
-    (:primitive? t)))
+    (= EnumSchema (type t))))
 
 (defmacro defenum
   [name & values]
   `(def ~name
-     {:primitive? true
-      ;; TODO: do something with the values (validation)
-      :values (vector ~@values)}))
+     (s/enum ~@values)))
 
 (defmacro deftype
   [name t]
