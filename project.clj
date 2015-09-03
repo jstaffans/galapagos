6667438ee4f04c962585ce819508cb4a59427733
jstaffans <johannes.staffans@gmail.com>
2015-09-03 08:52:13 +0200

    Update Prismatic Schema version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.wtfpl.net"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [instaparse "1.4.1"]
-                 [prismatic/schema "0.4.3"]
+                 [prismatic/schema "1.0.0"]
                  [muse "0.4.0"]
                  [cats "0.4.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
