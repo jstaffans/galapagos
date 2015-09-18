cd29130de50e58c14e9cc076e021109fd1f70972
jstaffans <johannes.staffans@gmail.com>
2015-09-18 22:38:09 +0200

    Test for type kinds

---
+++ test/galapagos/introspection_test.clj
@@ -0,0 +1,17 @@
+(ns galapagos.introspection-test
+  (:require [clojure.test :refer :all]
+            [juxt.iota :refer [given]]
+            [galapagos.introspection :as introspectiona]
+            [galapagos.schema.blog :refer [QueryRoot]]))
+
+
+(deftest introspection-test
+
+  (testing "Type kinds"
+    (given (introspectiona/type-map QueryRoot)
+      :BlogUser         :⊃ {:__kind :INTERFACE}
+      :Blogger          :⊃ {:__kind :UNION}
+      :Post             :⊃ {:__kind :OBJECT}
+      :Author           :⊃ {:__kind :OBJECT}
+      :PublishingDate   :⊃ {:__kind :SCALAR}
+      :PreferredEditor  :⊃ {:__kind :ENUM})))
