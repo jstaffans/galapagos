de88a2adb8536b67c857c8906751a7c0fb673e19
jstaffans <johannes.staffans@gmail.com>
2015-09-18 22:43:11 +0200

    Typofix

--- test/galapagos/introspection_test.clj
+++ test/galapagos/introspection_test.clj
@@ -1,14 +1,14 @@
 (ns galapagos.introspection-test
   (:require [clojure.test :refer :all]
             [juxt.iota :refer [given]]
-            [galapagos.introspection :as introspectiona]
+            [galapagos.introspection :as introspection]
             [galapagos.schema.blog :refer [QueryRoot]]))
 
 
 (deftest introspection-test
 
   (testing "Type kinds"
-    (given (introspectiona/type-map QueryRoot)
+    (given (introspection/type-map QueryRoot)
       :BlogUser         :⊃ {:__kind :INTERFACE}
       :Blogger          :⊃ {:__kind :UNION}
       :Post             :⊃ {:__kind :OBJECT}
