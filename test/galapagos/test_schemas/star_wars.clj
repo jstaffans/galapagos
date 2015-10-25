6f141b787e09fe980ad411d8572562ba5df6bd64
jstaffans <johannes.staffans@gmail.com>
2015-10-25 10:35:58 +0100

    Data update

--- test/galapagos/test_schemas/star_wars.clj
+++ test/galapagos/test_schemas/star_wars.clj
@@ -4,33 +4,39 @@
 
 (def data
   (atom
-    {1000 {:name        "Luke Skywalker"
-           :friends     [1002 1003 2000 2001]
-           :appears-in  [:a-new-hope :empire-strikes-back :return-of-the-jedi]
-           :home-planet "Tatooine"}
-     1001 {:name        "Vader"
-           :friends     [1004]
-           :appears-in  [:a-new-hope :empire-strikes-back :return-of-the-jedi]
-           :home-planet "Tatooine"}
-     1002 {:name       "Han Solo"
-           :friends    [1000 1003 2001]
-           :appears-in [:a-new-hope :empire-strikes-back :return-of-the-jedi]}
-     1003 {:name       "Leia"
-           :friends    [1000 1002 2000 2001]
-           :appears-in [:a-new-hope :empire-strikes-back :return-of-the-jedi]}
-     1004 {:name       "Wilhuff Tarkin"
-           :friends    [1001]
-           :appears-in [:a-new-hope]}
+    {1000 {:id         1000
+           :name       "Luke Skywalker"
+           :friends    [1002 1003 2000 2001]
+           :appearsIn  [:a-new-hope :empire-strikes-back :return-of-the-jedi]
+           :homePlanet "Tatooine"}
+     1001 {:id         1001
+           :name       "Vader"
+           :friends    [1004]
+           :appearsIn  [:a-new-hope :empire-strikes-back :return-of-the-jedi]
+           :homePlanet "Tatooine"}
+     1002 {:id        1002
+           :name      "Han Solo"
+           :friends   [1000 1003 2001]
+           :appearsIn [:a-new-hope :empire-strikes-back :return-of-the-jedi]}
+     1003 {:id        1003
+           :name      "Leia"
+           :friends   [1000 1002 2000 2001]
+           :appearsIn [:a-new-hope :empire-strikes-back :return-of-the-jedi]}
+     1004 {:id        1004
+           :name      "Wilhuff Tarkin"
+           :friends   [1001]
+           :appearsIn [:a-new-hope]}
 
-     2000 {:name             "C-3PO"
-           :friends          [1000 1002 1003 2001]
-           :appears-in       [:a-new-hope :empire-strikes-back :return-of-the-jedi]
-           :primary-function "Protocol"}
-     2001 {:name             "R2-D2"
-           :friends          [1000 1002 1003]
-           :appears-in       [:a-new-hope :empire-strikes-back :return-of-the-jedi]
-           :primary-function "Astromech"}}))
-
+     2000 {:id              2000
+           :name            "C-3PO"
+           :friends         [1000 1002 1003 2001]
+           :appearsIn       [:a-new-hope :empire-strikes-back :return-of-the-jedi]
+           :primaryFunction "Protocol"}
+     2001 {:id              2001
+           :name            "R2-D2"
+           :friends         [1000 1002 1003]
+           :appearsIn       [:a-new-hope :empire-strikes-back :return-of-the-jedi]
+           :primaryFunction "Astromech"}}))
 
 (schema/defenum Episode :a-new-hope :empire-strikes-back :return-of-the-jedi)
 
@@ -51,10 +57,10 @@
             ]})
 
 (schema/deftype Human [MovieCharacter]
-  {:fields [:home-planet schema/GraphQLString "The character's home planet"]})
+  {:fields [:homePlanet schema/GraphQLString "The character's home planet"]})
 
 (schema/deftype Droid [MovieCharacter]
-  {:fields [:primary-function schema/GraphQLString "The character's primary function"]})
+  {:fields [:primaryFunction schema/GraphQLString "The character's primary function"]})
 
 (defn- character-factory-fn
   [id]
